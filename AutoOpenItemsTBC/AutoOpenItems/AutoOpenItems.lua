9ee6922af7d5681679da65db62ee5852682ec962
Gogo1951 <55365231+Gogo1951@users.noreply.github.com>
2022-03-22 22:32:51 -0500

    Removed Pirate's Footlocker...

--- AutoOpenItemsTBC/AutoOpenItems/AutoOpenItems.lua
+++ AutoOpenItemsTBC/AutoOpenItems/AutoOpenItems.lua
@@ -1,4 +1,5 @@
 -- Creating a script 
+
 local AutoOpenItems = CreateFrame('Frame')
 AutoOpenItems:SetScript('OnEvent', function(self, event, ...) self[event](...) end)
 
@@ -114,7 +115,6 @@ local Whitelist = {
 	[19154]			= true,			-- Outrider Basic Care Package
 	[19155]			= true,			-- Outrider Standard Care Package
 	[11912]			= true,			-- Package of Empty Ooze Containers
-	[9276]			= true,			-- Pirate's Footlocker
 	[22155]			= true,			-- Pledge of Adoration: Darnassus
 	[22154]			= true,			-- Pledge of Adoration: Ironforge
 	[21975]			= true,			-- Pledge of Adoration: Stormwind
